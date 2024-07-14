624448b03d513618266103e6b06fca4b0b7b9fe6
scrahe <derekneubert11@gmail.com>
2024-07-14 18:24:14 -0500

    I broke something

--- src/main.c
+++ src/main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <o>
 
 #include "common.h"
 
 int main(int argc, char** argv) {
     if (argc == 1) {
-        fprintf(stderr, "%s: %serror:%s No input files\n", argv[0]);
+        fprintf(stderr, "%s: %serror:%s No input files\n", argv[0], BHRED, reset);
         exit(1);
     }
 }
