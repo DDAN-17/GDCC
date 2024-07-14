9020d01a70a10c1ef7a6f3964470b62da258d668
scrahe <derekneubert11@gmail.com>
2024-07-14 08:29:46 -0500

    Initial Commit

---
+++ GDC/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  GDC
+//
+//  Created by Derek Neubert on 7/14/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
